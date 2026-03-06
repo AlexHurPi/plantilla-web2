33c3a5d9938f40737a95478fcab69e86bfc7185b
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2026-03-05 20:50:20 -0500

    commit8

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/plantilla-web2/'  // Reemplaza con el nombre exacto de tu repo
+  base: '/plantilla-web2/' //Nombre exacto de tu repo
 })
 
